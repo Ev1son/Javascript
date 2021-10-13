072f04cf47de2417292cfdf126d90a4ce2edde0a
Ev1son <evison.pacheco21@gmail.com>
2021-10-13 12:58:06 -0300

    add: desafio 03

---
+++ Desafios/des03/script.js
@@ -0,0 +1,7 @@
+/* Auto incrementação não funcionou */
+
+function teste(){
+    var numtxt = prompt('Digite um número inteiro qualquer:')
+    var num = parseInt(numtxt)
+    alert(`Antes do ${num} temos o número ${num-1}.\nDepois do ${num} temos o número ${num+1}.`)
+}
