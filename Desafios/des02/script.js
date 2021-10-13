ca7822a885daa7eb31041bd0970e39eec0c39517
Ev1son <evison.pacheco21@gmail.com>
2021-10-13 12:14:25 -0300

    add: desafio 02

---
+++ Desafios/des02/script.js
@@ -0,0 +1,5 @@
+function conhecer(){
+    var nome = prompt('Qual o seu nome ?')
+    var cidade = prompt(`Olá ${nome}! Onde você mora ?`)
+    alert(`É um grande prazer te conhecer ${nome} de ${cidade}!`)
+}
