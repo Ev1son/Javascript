7984f3c7fe38da3f0fba7f71fbd651c2a8487660
Ev1son <evison.pacheco21@gmail.com>
2021-10-13 12:06:54 -0300

    add: desafio 01

---
+++ Desafios/des01/script.js
@@ -0,0 +1,11 @@
+function clique1() {
+    alert('Você clicou no Botão 1!')
+}
+
+function clique2(){
+    alert('Você clicou no Botão 2!')
+}
+
+function clique3(){
+    alert('Você clicou no Botão 3!')
+}
