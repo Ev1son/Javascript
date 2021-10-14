e8a4f8f458393361416c126055a2a7c66fb2b2b3
Ev1son <evison.pacheco21@gmail.com>
2021-10-14 16:28:22 -0300

    add: aula13

---
+++ Exercícios das Aulas/aula13_Repetições/ex015.js
@@ -0,0 +1,8 @@
+/* Estrutura de repetição com teste lógico no início
+Primeiro testa e depois executa o bloco */
+
+var c = 1
+while (c <= 6){
+    console.log('Hello, World!')
+    c++
+}
---
+++ Exercícios das Aulas/aula13_Repetições/ex016.js
@@ -0,0 +1,8 @@
+/* Estrutura de repetição com teste lógico no final
+Primeiro executa o bloco e depois testa */
+
+var c = 1
+do {
+    console.log('Hello, World!')
+    c++
+} while (c <= 6)
