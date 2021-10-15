321f29c75c41da861a8464a973e72b841ade0019
Ev1son <evison.pacheco21@gmail.com>
2021-10-15 16:54:43 -0300

    improvement:  mudei a forma de mostrar a tabuada
    Agora ao invés de inserir os resultados em um parágrafo coloquei para mostrar em um select

--- Exercícios Propostos/ex04/script.js
+++ Exercícios Propostos/ex04/script.js
@@ -1,23 +1,20 @@
 function calcular() {
 
+    var tabuada = document.getElementById('tabuada')
     var numtxt = document.getElementById('numtxt')
-    var num = Number(numtxt.value)
+    var num = Number(numtxt.value) //conversão de string para number
+    var c = 1
 
-    if (num == '') {
+    if (numtxt.value.length == 0) {
         alert('Insira um valor para calcular!')
     } else {
-        res.innerHTML = ''
+        tabuada.innerHTML = ''
+    }
+    
+    for (c; c <= 10; c++) {
 
-        res.innerHTML += `${num} x 0 = ${num * 0} <br>`
-        res.innerHTML += `${num} x 1 = ${num * 1} <br>`
-        res.innerHTML += `${num} x 2 = ${num * 2} <br>`
-        res.innerHTML += `${num} x 3 = ${num * 3} <br>`
-        res.innerHTML += `${num} x 4 = ${num * 4} <br>`
-        res.innerHTML += `${num} x 5 = ${num * 5} <br>`
-        res.innerHTML += `${num} x 6 = ${num * 6} <br>`
-        res.innerHTML += `${num} x 7 = ${num * 7} <br>`
-        res.innerHTML += `${num} x 8 = ${num * 8} <br>`
-        res.innerHTML += `${num} x 9 = ${num * 9} <br>`
-        res.innerHTML += `${num} x 10 = ${num * 10} <br>`
+        var item = document.createElement('option')
+        item.text = `${num} x ${c} = ${num * c}`
+        tabuada.appendChild(item)
     }
 }
