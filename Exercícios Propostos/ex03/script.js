fe922d46ea60ebfc57282ff4318deeadc38c0930
Ev1son <evison.pacheco21@gmail.com>
2021-10-15 00:39:58 -0300

    update: exercício proposto 3 antes da correção
    Código antes da correção

--- Exercícios Propostos/ex03/script.js
+++ Exercícios Propostos/ex03/script.js
@@ -1,4 +1,4 @@
-function contar(){
+function contar() {
     var iniciotxt = document.getElementById('ini')
     var inicio = Number(iniciotxt.value)
 
@@ -7,6 +7,33 @@ function contar(){
 
     var passotxt = document.getElementById('pas')
     var passo = Number(passotxt.value)
-
-    alert(`mostrar ${inicio}, ${fim} e ${passo}`) //teste das variáveis
+    if (fim == '') {
+        var pNovo = document.createElement('p')
+        var pConteudo = document.createTextNode(`Impossível contar sem "Fim"`)
+        pNovo.appendChild(pConteudo)
+        document.getElementById('res').appendChild(pNovo)
+    }
+    if (inicio == '') {
+        inicio = 1
+        alert('Início não pode ser vazio. Considerando Início = 1')
+    }
+    if (passo == '') {
+        passo = 1
+        alert('Passo não pode ser vazio. Considerando Passo = 1')
+    }
+    if (inicio > fim) {
+        for (inicio; inicio >= fim; inicio = inicio - passo) {
+            var pNovo = document.createElement('p')
+            var pConteudo = document.createTextNode(`${inicio} `)
+            pNovo.appendChild(pConteudo)
+            document.getElementById('res').appendChild(pNovo)
+        }
+    } else {
+        for (inicio; inicio <= fim; inicio = inicio + passo) {
+            var pNovo = document.createElement('p')
+            var pConteudo = document.createTextNode(`${inicio} `)
+            pNovo.appendChild(pConteudo)
+            document.getElementById('res').appendChild(pNovo)
+        }
+    }
 }
