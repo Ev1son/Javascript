dabc6e94cd1bdefcdc463078dbd4438341db12f0
Ev1son <evison.pacheco21@gmail.com>
2021-10-15 14:19:37 -0300

    fix: variáveis res e titulo
    Variáveis 'res' e 'título' removidas do laço. Desperdício de memória estarem dentro do laço

--- Exercícios Propostos/ex03/script.js
+++ Exercícios Propostos/ex03/script.js
@@ -8,6 +8,10 @@ function contar() {
     var passotxt = document.getElementById('pas')
     var passo = Number(passotxt.value)
 
+    var res = document.getElementById('res')
+
+    var titulo = document.getElementById('titulo')
+
     if (inicio == '') { //se início vazio
         inicio = 1
         alert('Início não pode ser vazio. Considerando Início = 1')
@@ -17,7 +21,7 @@ function contar() {
         alert('Passo não pode ser vazio. Considerando Passo = 1')
     }
     if (inicio > fim) { //se inicio for maior que fim
-        var res = document.getElementById('res')
+        
         res.innerHTML = '' //Limpa a div 'res' para não aparecer a contagem antiga
         if (fim == '') { //se fim for vazio retorna mensagem de erro
             var pNovo = document.createElement('p')
@@ -25,11 +29,9 @@ function contar() {
             pNovo.appendChild(pConteudo)
             document.getElementById('res').appendChild(pNovo)
         } else {
-            var res = document.getElementById('res')
             res.innerHTML = ''
 
             for (inicio; inicio >= fim; inicio = inicio - passo) {
-                var titulo = document.getElementById('titulo')
                 titulo.innerHTML = `Contando ...` //substitui o conteúdo do h2 'título'
 
                 var pNovo = document.createElement('p')
@@ -39,18 +41,15 @@ function contar() {
             }
         }
     } else {
-        var res = document.getElementById('res')
         res.innerHTML = ''
 
         for (inicio; inicio <= fim; inicio = inicio + passo) {
-            var titulo = document.getElementById('titulo')
             titulo.innerHTML = `Contando ...` 
 
             var pNovo = document.createElement('p')
             var pConteudo = document.createTextNode(`${inicio} `)
             pNovo.appendChild(pConteudo)
             document.getElementById('res').appendChild(pNovo)
-            var titulo = document.getElementById('titulo')
             titulo.innerHTML = `Contando ...`
         }
     }
