e4c39d6fe6a2f573444672f8a87255cdc7c00e91
Ev1son <evison.pacheco21@gmail.com>
2021-10-15 14:37:29 -0300

    improvement: melhoria na forma como a contagem é mostrada
    Ao invés de adicionar vários parágrafos um após o outro. Mudei para adicionar o conteúdo novo a um único parágrafo.

--- Exercícios Propostos/ex03/script.js
+++ Exercícios Propostos/ex03/script.js
@@ -1,6 +1,6 @@
 function contar() {
     var iniciotxt = document.getElementById('ini')
-    var inicio = Number(iniciotxt.value)
+    var inicio = Number(iniciotxt.value) //conversão para number
 
     var fimtxt = document.getElementById('fim')
     var fim = Number(fimtxt.value)
@@ -21,36 +21,27 @@ function contar() {
         alert('Passo não pode ser vazio. Considerando Passo = 1')
     }
     if (inicio > fim) { //se inicio for maior que fim
-        
+
         res.innerHTML = '' //Limpa a div 'res' para não aparecer a contagem antiga
         if (fim == '') { //se fim for vazio retorna mensagem de erro
-            var pNovo = document.createElement('p')
-            var pConteudo = document.createTextNode(`Impossível contar sem "Fim"`)
-            pNovo.appendChild(pConteudo)
-            document.getElementById('res').appendChild(pNovo)
+
+            res.innerHTML += `Impossível contar sem "Fim"! `
         } else {
             res.innerHTML = ''
 
             for (inicio; inicio >= fim; inicio = inicio - passo) {
                 titulo.innerHTML = `Contando ...` //substitui o conteúdo do h2 'título'
 
-                var pNovo = document.createElement('p')
-                var pConteudo = document.createTextNode(`${inicio} `)
-                pNovo.appendChild(pConteudo)
-                document.getElementById('res').appendChild(pNovo)
+                res.innerHTML += `${inicio} `
             }
         }
     } else {
         res.innerHTML = ''
 
         for (inicio; inicio <= fim; inicio = inicio + passo) {
-            titulo.innerHTML = `Contando ...` 
-
-            var pNovo = document.createElement('p')
-            var pConteudo = document.createTextNode(`${inicio} `)
-            pNovo.appendChild(pConteudo)
-            document.getElementById('res').appendChild(pNovo)
             titulo.innerHTML = `Contando ...`
+
+            res.innerHTML += `${inicio} `
         }
     }
 }
