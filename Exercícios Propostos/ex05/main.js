a9b7ecabe0d2f0edd12bc8ca9856727650a4ba5c
Ev1son <evison.pacheco21@gmail.com>
2021-10-28 15:44:17 -0300

    create: validation functions

--- Exercícios Propostos/ex05/main.js
+++ Exercícios Propostos/ex05/main.js
@@ -1,23 +1,40 @@
-let numberValue = document.getElementById('inputAddNumber')
-let selectNumberList = document.getElementById('selectNumberList')
+let numberValueTxt = document.getElementById('inputAddNumber')
 
-var arrayValues = []
+let arrayValues = []
 
 document.getElementById('addNumberButton').addEventListener('click', addNumberToArray)
 
 function addNumberToArray() {
   numberValue = document.getElementById('inputAddNumber')
-  if (numberValue.value >= 1 && numberValue.value <= 100) {
-    arrayValues.push(Number(numberValue.value))
+  if (checkNumber(true) && checkList(true)) {
+    arrayValues.push(numberValue.valueAsNumber)
     console.log(arrayValues)
     addValueToList()
   } else {
-    alert('Valor Inválido!')
+    alert('Valor Inválido! Verifique os dados.')
   }
 }
 
 function addValueToList() {
+  let selectNumberList = document.getElementById('selectNumberList')
   let option = document.createElement('option')
-  option.innerHTML = `O valor ${numberValue.value} foi adicionado`
+
+  option.innerHTML = `O valor ${numberValue.valueAsNumber} foi adicionado`
   selectNumberList.appendChild(option)
+}
+
+function checkNumber() {
+  if (numberValue.valueAsNumber >= 1 && numberValue.valueAsNumber <= 100) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function checkList() {
+  if (arrayValues.indexOf(numberValue.valueAsNumber) == -1) {
+    return true
+  } else {
+    return false
+  }
 }
