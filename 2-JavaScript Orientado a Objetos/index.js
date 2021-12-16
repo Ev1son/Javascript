738b47b45018d52f13df4bf2bce5deb030070cea
Ev1son <evison.pacheco21@gmail.com>
2021-12-15 23:07:59 -0300

    update: deixando o atributo 'saldo' privado

--- 2-JavaScript Orientado a Objetos/index.js
+++ 2-JavaScript Orientado a Objetos/index.js
@@ -1,32 +1,31 @@
 class Client {
   nome;
   cpf;
-
 }
 
 class CheckingAccount {
   agencia;
-  saldo;
+  _saldo = 0;
 
   sacar(valor) {
-    console.log(`Saldo atual: ${this.saldo} reais`);
+    console.log(`Saldo atual: ${this._saldo} reais`);
     console.log(`Você está tentando sacar ${valor} reais`);
-    if (this.saldo >= valor) {
-      this.saldo -= valor;
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
       console.log("Saque Efetuado.");
-      console.log(`Seu novo saldo é: ${this.saldo} reais`);
+      console.log(`Seu novo saldo é: ${this._saldo} reais`);
     } else {
       console.log("Saldo Insuficiente.");
     }
   }
 
   depositar(valor) {
-    console.log(`Saldo atual: ${this.saldo} reais`);
+    console.log(`Saldo atual: ${this._saldo} reais`);
     console.log(`Você está tentando depositar ${valor} reais`);
     if (valor > 0) {
       this.saldo += valor;
       console.log(`Depósito Efetuado.`);
-      console.log(`Seu novo saldo é: ${this.saldo} reais`);
+      console.log(`Seu novo saldo é: ${this._saldo} reais`);
     } else {
       console.log("Erro ao tentar depositar.")
     }
@@ -42,10 +41,9 @@ client2.nome = "Viviane";
 client2.cpf = 99988877701;
 
 const contaCorrente1 = new CheckingAccount();
-contaCorrente1.saldo = 80;
 contaCorrente1.agencia = 1001;
 
-/* contaCorrente1.sacar(50);
-contaCorrente1.depositar(100); */
+contaCorrente1.sacar(50);
+/* contaCorrente1.depositar(100); */ 
 
 console.log(contaCorrente1);
