53b84a15cabc676754242c1099f675a1fd8bf798
Ev1son <evison.pacheco21@gmail.com>
2021-12-15 22:37:11 -0300

    update: método depositar criado

--- 2-JavaScript Orientado a Objetos/index.js
+++ 2-JavaScript Orientado a Objetos/index.js
@@ -9,13 +9,26 @@ class CheckingAccount {
   saldo;
 
   sacar(valor) {
-    console.log(`Você está tentando sacar ${valor} reais`)
-    if(this.saldo >= valor){
+    console.log(`Saldo atual: ${this.saldo} reais`);
+    console.log(`Você está tentando sacar ${valor} reais`);
+    if (this.saldo >= valor) {
       this.saldo -= valor;
       console.log("Saque Efetuado.");
-      console.log(`Seu novo saldo é: ${valor} reais`);
+      console.log(`Seu novo saldo é: ${this.saldo} reais`);
     } else {
       console.log("Saldo Insuficiente.");
+    }
+  }
+
+  depositar(valor) {
+    console.log(`Saldo atual: ${this.saldo} reais`);
+    console.log(`Você está tentando depositar ${valor} reais`);
+    if (valor > 0) {
+      this.saldo += valor;
+      console.log(`Depósito Efetuado.`);
+      console.log(`Seu novo saldo é: ${this.saldo} reais`);
+    } else {
+      console.log("Erro ao tentar depositar.")
     }
   }
 }
@@ -29,10 +42,10 @@ client2.nome = "Viviane";
 client2.cpf = 99988877701;
 
 const contaCorrente1 = new CheckingAccount();
-contaCorrente1.saldo = 100;
+contaCorrente1.saldo = 80;
 contaCorrente1.agencia = 1001;
 
-contaCorrente1.sacar(50);
+/* contaCorrente1.sacar(50);
+contaCorrente1.depositar(100); */
 
-/* console.log(client1);
-console.log(client2); */
+console.log(contaCorrente1);
