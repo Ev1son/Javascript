c59d32891e9e956a7816064e78994c0a4cf225b8
Ev1son <evison.pacheco21@gmail.com>
2021-12-22 20:26:03 -0300

    create: SavingAccount.js

---
+++ 3-JavaScript Interfaces e Herança/class/SavingsAccount.js
@@ -0,0 +1,41 @@
+export class SavingsAccount {
+  constructor (initialBalance, client, agency){
+    this._balance = initialBalance;
+    this._client = client;
+    this._agency = agency;
+  }
+
+  withdraw(amount) {
+    if (this._balance >= amount) {
+      this._balance -= amount;
+      console.log(`\nSaque de ${amount} reais realizado com sucesso.\n`);
+      return amount;
+    }
+  }
+
+  deposit(amount) {
+    if (amount <= 0) return console.log("\nValor inválido.\n");
+    this._balance += amount;
+    console.log(`\nDepósito de ${amount} reais realizado com sucesso.\n`);
+  }
+
+  withdrawNoMessage(amount) {
+    if (this._balance >= amount) {
+      this._balance -= amount;
+      return amount;
+    }
+  }
+
+  depositNoMessage(amount) {
+    this._balance += amount;
+  }
+
+  transfer(amount, account) {
+    if (amount > this._balance) return console.log("\nO saldo dessa conta é insuficiente.\n");
+    if (amount <= 0) return console.log("\nValor inválido.\n");
+
+    this.withdrawNoMessage(amount);
+    account.depositNoMessage(amount);
+    console.log(`\nTransferência de ${amount} reais de ${this.client.name} para ${account.client.name} realizado com sucesso.\n`);
+  }
+}
