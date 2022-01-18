e46727684d138b7ed85144b2ba9017ec3049d6ad
Ev1son <evison.pacheco21@gmail.com>
2022-01-18 16:37:16 -0300

    feature: taxa de saque adicionada em 'SavingsAccount'

--- 3-JavaScript Interfaces e Herança/class/SavingsAccount.js
+++ 3-JavaScript Interfaces e Herança/class/SavingsAccount.js
@@ -4,4 +4,10 @@ export class SavingsAccount extends Account {
   constructor (agency, client, initialBalance){
     super(agency, client, initialBalance);
   }
+
+  withdraw(amount){
+    const rate = 1.02;
+    return this._withdraw(rate, amount);
+  }
+
 }
